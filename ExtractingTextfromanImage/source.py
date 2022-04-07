d1bb301f7a1596e0ac324dfe67479ad97d669134
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2022-04-07 13:20:28 +0500

    feat

--- ExtractingTextfromanImage/source.py
+++ ExtractingTextfromanImage/source.py
@@ -18,6 +18,20 @@ directory = r'D:\SIXTHSEMESTERMATERIAL\CVIP\Assignments\ExtractingTextfromanImag
 
 img = cv2.imread(path)
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+#we will print first
+#print(pytesseract.image_to_string(img))
+
+### Detecting Characters
+hImg,wImg,_ = img.shape
+boxes = pytesseract.image_to_boxes(img)
+for b in boxes.splitlines():
+    #print(b)
+    b = b.split(' ')
+    print(b)
+    x,y,w,h = int(b[1]),int(b[2]),int(b[3]),int(b[4])
+    cv2.rectangle(img,(x,hImg-y),(w,hImg-h),(0,0,255),1)
+
+
 cv2.imshow('Result',img)
 cv2.waitKey(0)
 
