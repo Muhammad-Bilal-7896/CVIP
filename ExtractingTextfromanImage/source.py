c9fb8eb39bafcc62c1c8ffee48b752e7d549ff26
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2022-04-07 13:56:44 +0500

    feat

--- ExtractingTextfromanImage/source.py
+++ ExtractingTextfromanImage/source.py
@@ -7,13 +7,13 @@ def save_to_directory(p,d):
     os.chdir(d) 
     print("Before saving")   
     print(os.listdir(d))   
-    filename = 'newFile.png'
+    filename = 'textDetected.png'
     cv2.imwrite(filename, img) 
     print("After saving")  
     print(os.listdir(d))
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-path = r'D:\SIXTHSEMESTERMATERIAL\CVIP\Assignments\ExtractingTextfromanImage\Input\textImage2.png'
+path = r'D:\SIXTHSEMESTERMATERIAL\CVIP\Assignments\ExtractingTextfromanImage\Input\textImage.png'
 directory = r'D:\SIXTHSEMESTERMATERIAL\CVIP\Assignments\ExtractingTextfromanImage\Output'
 
 img = cv2.imread(path)
@@ -30,6 +30,9 @@ for b in boxes.splitlines():
     print(b)
     x,y,w,h = int(b[1]),int(b[2]),int(b[3]),int(b[4])
     cv2.rectangle(img,(x,hImg-y),(w,hImg-h),(0,0,255),1)
+    cv2.putText(img,b[0],(x,hImg-y+20),cv2.FONT_HERSHEY_COMPLEX,1,(50,50,255),1)
+
+save_to_directory(path,directory)
 
 
 cv2.imshow('Result',img)
